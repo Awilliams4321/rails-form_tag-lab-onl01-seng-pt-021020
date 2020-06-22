7ec023a1b96e527f4b1953bfd1a8c98fd13b4915
Awilliams4321 <awilliams1234@live.com>
2020-06-22 12:18:20 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,13 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    Student.new(first_name: params[:first_name], last_name: params[:last_name])
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end 
+
 end
